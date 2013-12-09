858a88093725d85eac6b5e85e97fdd8f13743716
Daniel Hedenius <daniel.hedenius@gmail.com>
2013-12-09 08:41:47 +0100

    Added initial version of Tips & Trix

--- js/tips.js
+++ js/tips.js
@@ -12,18 +12,50 @@ window.TeamFinder.Tips = (function (Tips) {
 						$(this).parent().children('.navigateRSubMenu').slideToggle();
 					});
 				});
+				
+				$('.tipsContent').each(function () {
+					$(this).on('click', function () {
+						$('.dataContainer').each(function () {
+							$(this).hide();
+						});
+						
+						$('#' + $(this).data('envido-container')).fadeIn();
+					});
+				});
 			}, 'obj => !TeamFinder.Utils.notNullOrUndefinedFunction(obj.navigateR)', $('.navigateRMenu'), 1);
 		};
 		
 		return Callbacks;
 	}(Tips.Callbacks || {}));
 	
+	Tips.Elements = (function (Elements) {
+		Elements.divFloorballRulesContainer = null;
+		Elements.divSoccerRulesContainer = null;
+		
+		Elements.initialize = function () {
+			for (var key in this) {
+				if ('function' !== typeof this[key]) {
+					this[key] = $('#' + key);
+				}
+			}
+			
+			Elements.dataContainers = $('.dataContainer');
+		};
+		
+		return Elements;
+	}(Tips.Elements || {}));
+	
 	Tips.initialize = function () {
+		Tips.Elements.initialize();
+		
 		TeamFinder.callServer('../data/tipsNavigation.html', '', 'GET', 'html', Tips.Callbacks.navigation, TeamFinder.handleError);
+		
+		Tips.Elements.dataContainers.hide();
 	};
 	
 	Tips.loadDependencies = function () {
 		TeamFinder.loadStyle('../css/jquery.navigateR.css', null);
+		TeamFinder.loadStyle('../css/tips.css', null);
 		
 		TeamFinder.loadScript('../js/jquery.navigateR.js', null);
 	};
